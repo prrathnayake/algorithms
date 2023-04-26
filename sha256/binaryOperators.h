96894a388b4cee7c808a6656d89c4b8d1fdccb02
prrathnayake <pasanransika1@gmail.com>
2023-04-26 22:28:00 +0530

    add XNOR and NOT operators

---
+++ sha256/binaryOperators.h
@@ -0,0 +1,108 @@
+#pragma once
+#include <iostream>
+
+void sizeValidate(std::string *binary1, std::string *binary2)
+{
+    std::string add;
+    int bin1Len = binary1->size();
+    int bin2Len = binary2->size();
+
+    if (bin1Len < bin2Len)
+    {
+        for (int i = 0; i < (bin2Len - bin1Len); i++)
+        {
+            add += "0";
+        }
+        *binary1 = add + *binary1;
+    }
+    else if (bin2Len < bin1Len)
+    {
+        for (int i = 0; i < (bin1Len - bin2Len); i++)
+        {
+            add += "0";
+        }
+        *binary2 = add + *binary2;
+    }
+}
+
+std::string AND(std::string binary1, std::string binary2)
+{
+    sizeValidate(&binary1, &binary2);
+
+    std::string result;
+
+    for (int i = 1; i <= binary1.size(); i++)
+    {
+        if (binary1[i] == '1' & binary2[i] == '1')
+        {
+            result += '1';
+        }
+        else
+        {
+            result += '0';
+        }
+    }
+
+    return result;
+}
+
+std::string OR(std::string binary1, std::string binary2)
+{
+    sizeValidate(&binary1, &binary2);
+
+    std::string result;
+
+    for (int i = 1; i <= binary1.size(); i++)
+    {
+        if (binary1[i] == '1' || binary2[i] == '1')
+        {
+            result += '1';
+        }
+        else
+        {
+            result += '0';
+        }
+    }
+
+    return result;
+}
+
+std::string XNOR(std::string binary1, std::string binary2)
+{
+    sizeValidate(&binary1, &binary2);
+
+    std::string result;
+
+    for (int i = 1; i <= binary1.size(); i++)
+    {
+        if (binary1[i] == binary2[i])
+        {
+            result += '1';
+        }
+        else
+        {
+            result += '0';
+        }
+    }
+
+    return result;
+}
+
+std::string NOT(std::string binary)
+{
+    std::string result;
+
+    for (int i = 1; i <= binary.size(); i++)
+    {
+        if (binary[i] == '1')
+        {
+            result += '0';
+        }
+        else
+        {
+            result += '1';
+        }
+    }
+
+    return result;
+}
--- sha256/common.h
+++ sha256/common.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream>
-#include <string>
 #include <bitset>
 
 std::string toBinary(std::string const &str)
@@ -11,70 +10,4 @@ std::string toBinary(std::string const &str)
         binary += std::bitset<8>(c).to_string();
     }
     return binary;
-}
-
-void sizeValidate(std::string *binary1, std::string *binary2)
-{
-    std::string add;
-    int bin1Len = binary1->size();
-    int bin2Len = binary2->size();
-
-    if (bin1Len < bin2Len)
-    {
-        for (int i = 0; i < (bin2Len - bin1Len); i++)
-        {
-            add += "0";
-        }
-        *binary1 = add + *binary1;
-    }
-    else if (bin2Len < bin1Len)
-    {
-        for (int i = 0; i < (bin1Len - bin2Len); i++)
-        {
-            add += "0";
-        }
-        *binary2 = add + *binary2;
-    }
-}
-
-std::string AND(std::string binary1, std::string binary2)
-{
-    sizeValidate(&binary1, &binary2);
-
-    std::string result;
-
-    for (int i = 1; i <= binary1.size(); i++)
-    {
-        if (binary1[i] == '1' & binary2[i] == '1')
-        {
-            result += '1';
-        }
-        else
-        {
-            result += '0';
-        }
-    }
-
-    return result;
-}
-
-std::string OR(std::string binary1, std::string binary2)
-{
-    sizeValidate(&binary1, &binary2);
-
-    std::string result;
-
-    for (int i = 1; i <= binary1.size(); i++)
-    {
-        if (binary1[i] == '1' || binary2[i] == '1')
-        {
-            result += '1';
-        }
-        else
-        {
-            result += '0';
-        }
-    }
-
-    return result;
 }
